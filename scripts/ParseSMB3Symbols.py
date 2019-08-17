21773c187133d24b19956da6215bc58b7e944d09
fortenbt <1106428+fortenbt@users.noreply.github.com>
2019-08-16 21:30:09 -0400

    Parse all symbols and create java classes

--- scripts/ParseSMB3Symbols.py
+++ scripts/ParseSMB3Symbols.py
@@ -8,15 +8,22 @@ import sys
 SMB3_FNS_FILE       = 'smb3.fns'
 SMB3_PRG_TEMPLATE   = 'prg{:03}.asm'
 
+JAVA_SYMBOL_TEMPLATE = '''
+package inesloader;
+
+import inesloader.SMB3Symbols;
+import inesloader.SMB3Symbols.BankSymbol;
+
+public class SMB3AutoSymsBank{0} {{
+    public static final BankSymbol[] SMB3_BANK{0:03}_SYMS = {{
+'''
 
 def main(sysargs):
     parser = argparse.ArgumentParser(description='Parse out all the symbols within SMB3\'s asm files.')
     parser.add_argument('disasmdir', help='Path to the top-level directory of the SMB3 disassembly.')
-    parser.add_argument('-o', '--outfile', default='./ghidra-smb3-symbols.txt', help='File path for the output symbols.')
 
     args = parser.parse_args()
     args.disasmdir = os.path.realpath(args.disasmdir)
-    args.outfile = os.path.realpath(args.outfile)
 
     # Symbols and includes, but ignore commented lines
     # For symbols, we don't want ".func" macros (?! is a negative lookahead)
@@ -69,15 +76,26 @@ def main(sysargs):
 
     with open(fnsname, 'r') as f:
         fnslines = f.readlines()
-    with open(args.outfile, 'w') as f:
-        for s in syms:
-            try:
-                line = next(l for l in fnslines if s in l)
-                syms[s]['addr'] = int('0x'+line[-5:-1], 0)
 
-                f.write('new BankSymbol("{}", {}, 0x{:X}),\n'.format(s, syms[s]['bank'], syms[s]['addr']))
-            except StopIteration as e:
-                print('Did not find symbol "{}"'.format(s))
+    outfs = [None]*32
+    for i in range(len(outfs)):
+        outfs[i] = open(os.path.realpath('./SMB3AutoSymsBank{}.java'.format(i)), 'w')
+        outfs[i].write(JAVA_SYMBOL_TEMPLATE.format(i))
+
+
+    for s in syms:
+        try:
+            fnspat = '^{}[ ]+= \$[0-9A-F][0-9A-F][0-9A-F][0-9A-F]$'.format(s)
+            line = next(re.search(fnspat, l).group(0) for l in fnslines if re.search(fnspat, l) is not None)
+            syms[s]['addr'] = int('0x'+line[-4:], 0)
+
+            outfs[syms[s]['bank']].write('        new BankSymbol("{}", {}, 0x{:X}),\n'.format(s, syms[s]['bank'], syms[s]['addr']))
+        except StopIteration as e:
+            print('Did not find symbol "{}"'.format(s))
+
+    for i in range(len(outfs)):
+        outfs[i].write('    };\n}')
+        outfs[i].close()
 
     return 0
 
